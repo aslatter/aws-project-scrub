dc9272a2e95ed6506a3edc0b40910b8914112277
Antoine Latter <aslatter@gmail.com>
2024-11-26 10:08:58 -0600

    clean up load balancers

---
+++ resource/elb_load_balancer.go
@@ -0,0 +1,41 @@
+package resource
+
+import (
+	"aws-project-scrub/config"
+	"context"
+	"fmt"
+
+	elb "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
+)
+
+type elbLoadBalancer struct{}
+
+// DeleteResource implements ResourceProvider.
+func (e *elbLoadBalancer) DeleteResource(ctx context.Context, s *config.Settings, r Resource) error {
+	c := elb.NewFromConfig(s.AwsConfig)
+	_, err := c.DeleteLoadBalancer(ctx, &elb.DeleteLoadBalancerInput{
+		LoadBalancerArn: &r.ID[0],
+	})
+	if err != nil {
+		return err
+	}
+
+	w := elb.NewLoadBalancersDeletedWaiter(c)
+	err = w.Wait(ctx, &elb.DescribeLoadBalancersInput{
+		LoadBalancerArns: []string{r.ID[0]},
+	}, defaultDeleteWaitTime)
+	if err != nil {
+		return fmt.Errorf("waiting for load-balancer deletion: %s", err)
+	}
+
+	return nil
+}
+
+// Type implements ResourceProvider.
+func (e *elbLoadBalancer) Type() string {
+	return ResourceTypeLoadBalancer
+}
+
+func init() {
+	register(&elbLoadBalancer{})
+}
---
+++ resource/elb_target_group.go
@@ -0,0 +1,28 @@
+package resource
+
+import (
+	"aws-project-scrub/config"
+	"context"
+
+	elb "github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2"
+)
+
+type elbTargetGroup struct{}
+
+// DeleteResource implements ResourceProvider.
+func (e *elbTargetGroup) DeleteResource(ctx context.Context, s *config.Settings, r Resource) error {
+	c := elb.NewFromConfig(s.AwsConfig)
+	_, err := c.DeleteTargetGroup(ctx, &elb.DeleteTargetGroupInput{
+		TargetGroupArn: &r.ID[0],
+	})
+	return err
+}
+
+// Type implements ResourceProvider.
+func (e *elbTargetGroup) Type() string {
+	return ResourceTypeLoadBalancerTargetGroup
+}
+
+func init() {
+	register(&elbTargetGroup{})
+}
