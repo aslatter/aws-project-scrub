0ae1fc15dc5fe6b2a6b9fb51524f1d1f7038b26a
Antoine Latter <aslatter@gmail.com>
2024-12-27 10:06:19 -0600

    check for EC2-not-found errors

--- internal/resource/errors.go
+++ internal/resource/errors.go
@@ -2,14 +2,30 @@ package resource
 
 import (
 	"errors"
-
-	awshttp "github.com/aws/aws-sdk-go-v2/aws/transport/http"
 )
 
 func IsErrNotFound(err error) bool {
-	var httpResponseErr *awshttp.ResponseError
-	if errors.As(err, &httpResponseErr) {
-		return httpResponseErr.Response.StatusCode == 404
+	var httpResponseErr interface{ HTTPStatusCode() int }
+	if errors.As(err, &httpResponseErr) && httpResponseErr.HTTPStatusCode() == 404 {
+		return true
 	}
+
+	// EC2 resources seem to use status-code '400' for not-found errors, so we check for
+	// error-codes explicitly. This doesn't make sense for a general 'not found' check,
+	// but in the context of resource-deletions it is what we want.
+	//
+	// So far we've seen this with:
+	//  + Security Groups
+	//  + Volumes
+	//
+	// https://docs.aws.amazon.com/AWSEC2/latest/APIReference/errors-overview.html#CommonErrors
+	var apiError interface{ ErrorCode() string }
+	if errors.As(err, &apiError) {
+		switch apiError.ErrorCode() {
+		case "InvalidGroup.NotFound", "InvalidVolume.NotFound":
+			return true
+		}
+	}
+
 	return false
 }
